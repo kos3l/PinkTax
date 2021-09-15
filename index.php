8cb6b15b828eac969c6a867fd8754d2a57591466
kos3l <71730597+kos3l@users.noreply.github.com>
2021-09-15 13:31:00 +0200

    created the subpage first view

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php get_header() ?>
+            <?php if(have_posts()):?>
                 <?php while(have_posts()): the_post() ?>
-                <div class="row ">
-                    <div class="col-6 content" >
-                        <p><?php the_content() ?></p>  
-                        <img src="<?php the_field('big_logo') ?>" class="logo">
+                    <div class="row ">
+                        <div class="col-6 content" >
+                            <p><?php the_content() ?></p>  
+                            <img src="<?php the_field('big_logo') ?>" class="logo">
+                        </div>
+                        <div class="col-6" style="background-image: url(<?php the_field('right_image'); ?>);background-size:cover;background-position:50% 100%">
+                        </div>
                     </div>
-                    <div class="col-6" style="background-image: url(<?php the_field('right_image'); ?>);background-size:cover;background-position:50% 100%">
+                    <div class="row color-bar">
+                        <div class="col"></div>
                     </div>
-                </div>
-                <div class="row color-bar">
-                    <div class="col"></div>
-                </div>
                 <?php endwhile; ?>
+            <?php endif; ?>
 
 <?php get_footer() ?>
--- page.php
+++ page.php
@@ -1,5 +1,28 @@
 <?php get_header() ?>
-
+<?php if(have_posts()):?>
+                <?php while(have_posts()): the_post() ?>
+                    <div class="row upper">
+                        <div class="col-6 subpage" >
+                            <p><?php the_content() ?></p>  
+                        </div>
+                        <div class="col-6 scroll-container">
+                            <div class="inner-scroll-container">
+                                <div class="scroll-text"><img src="<?php the_field('arrow') ?>" alt=""> <p><?php the_field('scroll') ?></p></div>
+                                <div class="scroll-text"><img src="<?php the_field('arrow') ?>" alt=""> <p><?php the_field('scroll-2') ?></p></div>
+                                <div class="scroll-text"><img src="<?php the_field('arrow') ?>" alt=""> <p><?php the_field('scroll-3') ?></p></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row color-bar">
+                        <div class="col">
+                            <div class="scroll-text"><img src="<?php the_field('arrow') ?>" alt=""> <p><?php the_field('scroll') ?></p></div>
+                            <div class="scroll-text"><img src="<?php the_field('arrow') ?>" alt=""> <p><?php the_field('scroll') ?></p></div>
+                            <div class="scroll-text"><img src="<?php the_field('arrow') ?>" alt=""> <p><?php the_field('scroll') ?></p></div>
+                            <p><?php the_field('scroll') ?></p></div>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
 
 
 <?php get_footer() ?>
