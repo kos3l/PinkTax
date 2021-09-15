222d75e977777df9ae202f07df90a4963d791dea
kos3l <71730597+kos3l@users.noreply.github.com>
2021-09-15 16:26:47 +0200

    created sections on the subpage

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php get_header() ?>
             <?php if(have_posts()):?>
                 <?php while(have_posts()): the_post() ?>
-                    <div class="row ">
+                    <div class="row main">
                         <div class="col-6 content" >
                             <p><?php the_content() ?></p>  
                             <img src="<?php the_field('big_logo') ?>" class="logo">
                         </div>
-                        <div class="col-6" style="background-image: url(<?php the_field('right_image'); ?>);background-size:cover;background-position:50% 100%">
+                        <div class="col-6" style="background-image: url(<?php the_field('main_image'); ?>);background-size:cover;background-position:80% 100%">
                         </div>
                     </div>
                     <div class="row color-bar">
--- page.php
+++ page.php
@@ -14,15 +14,28 @@
                         </div>
                     </div>
                     <div class="row color-bar">
-                        <div class="col">
-                            <div class="scroll-text"><img src="<?php the_field('arrow') ?>" alt=""> <p><?php the_field('scroll') ?></p></div>
-                            <div class="scroll-text"><img src="<?php the_field('arrow') ?>" alt=""> <p><?php the_field('scroll') ?></p></div>
-                            <div class="scroll-text"><img src="<?php the_field('arrow') ?>" alt=""> <p><?php the_field('scroll') ?></p></div>
-                            <p><?php the_field('scroll') ?></p></div>
+                        <div class="col-6 bar">
+                            <div class="bar-inner">
+                                <img src="<?php the_field('icon') ?>" alt="">
+                                <img src="<?php the_field('icon2') ?>" alt=""> 
+                                <img src="<?php the_field('icon3') ?>" id="shirt" alt=""> 
+                            </div>
+                        </div>
+                        <div class="col-6 bar-text">
+                            <p><?php the_field('color_bar_text') ?></p>
                         </div>
                     </div>
+                    <div class="row main">
+                        <div class="col-6" style="background-image: url(<?php the_field('main_image'); ?>);background-size:cover;background-position:80% 100%"></div>
+                        <div class="col-6 section">
+                            <p class="title"><?php the_field('main-text') ?></p>
+                            <p class="intro"><?php the_field('intro') ?></p>
+                            <p class="body-text"><?php the_field('body') ?></p>
+
+                        </div>
+                    </div>
+
                 <?php endwhile; ?>
             <?php endif; ?>
-
 
 <?php get_footer() ?>
